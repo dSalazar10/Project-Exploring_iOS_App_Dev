a964c389214b957dda0443c0e73f6d126dddf4e4
Daniel Salazar <dsalazar10@horizon.csueastbay.edu>
2019-11-30 11:38:45 -0800

    Add files via upload

---
+++ Ch3/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Daniel Salazar on 11/30/19.
+//  Copyright © 2019 Daniel Salazar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        // Create a Frame
+//        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
+//        //  Create a View
+//        let firstView = UIView(frame: firstFrame)
+//        // Set View's color to blue
+//        firstView.backgroundColor = UIColor.blue
+//        // Add View to ViewControler's View
+//        view.addSubview(firstView)
+//
+//        let secondFrame = CGRect(x: 20, y: 30, width: 50, height: 50)
+//        let secondView = UIView(frame: secondFrame)
+//        secondView.backgroundColor = UIColor.green
+//        //view.addSubview(secondView)
+//        firstView.addSubview(secondView)
+    }
+
+
+}
+
