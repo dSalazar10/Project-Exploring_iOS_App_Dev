f5c42cda42c1506a319c22da95cd177d94ae696a
Daniel Salazar <dsalazar10@horizon.csueastbay.edu>
2019-11-30 18:05:30 -0800

    Add files via upload

---
+++ Ch4/ConversionViewController.swift
@@ -0,0 +1,108 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Daniel Salazar on 11/30/19.
+//  Copyright © 2019 Daniel Salazar. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController, UITextFieldDelegate {
+
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        // Display no more than 1 decimal place
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        // Property observer that calls function before value set
+        willSet {
+            // Do Nothing - for example
+        }
+        // Property observer that calls function after value set
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    var celsiusValue: Measurement<UnitTemperature>? {
+        // Check if a F value exists
+        if let fahrenheitValue = fahrenheitValue {
+            // Convert F to C
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
+    
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+//        if let text = textField.text, !text.isEmpty {
+//            celsiusLabel.text = text
+//        } else {
+//            celsiusLabel.text = "???"
+//        }
+        // Check if the text field has some text
+        // Next, check if value can be represented by a double
+        if let text =  textField.text, let value = Double(text) {
+            // Convert from text to double and set F value with result
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        } else {
+            fahrenheitValue = nil
+        }
+    }
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
+    
+    func updateCelsiusLabel() {
+        // Check if C value exists
+        if let celsiusValue = celsiusValue {
+//            // Set C label to C value
+//            celsiusLabel.text = "\(celsiusValue.value)"
+            // Use the number formatter to set the value
+            celsiusLabel.text = numberFormatter.string(from: NSNumber(value: celsiusValue.value))
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+//        print("Current text: \(String(describing: textField.text))")
+//        print("Replacement text: \(string)")
+//        return true
+        let existingTextHasDecimalSeparator = textField.text?.range(of: ".")
+        let replacementTextHasDecimalSeparator = string.range(of: ".")
+        if existingTextHasDecimalSeparator != nil,
+            replacementTextHasDecimalSeparator != nil {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateCelsiusLabel()
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
