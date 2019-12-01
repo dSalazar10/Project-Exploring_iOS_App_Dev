6d4268b81f718b697f37ff964ccfa674c32fa16d
Daniel Salazar <dsalazar10@horizon.csueastbay.edu>
2019-12-01 15:56:50 -0800

    Add files via upload

---
+++ Ch7/ConversionViewController.swift
@@ -0,0 +1,122 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Daniel Salazar on 11/30/19.
+//  Copyright © 2019 Daniel Salazar. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController, UITextFieldDelegate {
+
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        // Display no more than 1 decimal place
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        // Property observer that calls function before value set
+        willSet {
+            // Do Nothing - for example
+        }
+        // Property observer that calls function after value set
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    var celsiusValue: Measurement<UnitTemperature>? {
+        // Check if a F value exists
+        if let fahrenheitValue = fahrenheitValue {
+            // Convert F to C
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
+    
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+//        if let text = textField.text, !text.isEmpty {
+//            celsiusLabel.text = text
+//        } else {
+//            celsiusLabel.text = "???"
+//        }
+//        // Check if the text field has some text
+//        // Next, check if value can be represented by a double
+//        if let text =  textField.text, let value = Double(text) {
+//            // Convert from text to double and set F value with result
+//            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+//        } else {
+//            fahrenheitValue = nil
+//        }
+        // Convert value in a Locale independent way
+        if let text = textField.text, let number = numberFormatter.number(from: text) {
+            // Convert from a string to a double
+            fahrenheitValue = Measurement(value: number.doubleValue, unit: .fahrenheit)
+        } else {
+            fahrenheitValue = nil
+        }
+    }
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
+    
+    func updateCelsiusLabel() {
+        // Check if C value exists
+        if let celsiusValue = celsiusValue {
+//            // Set C label to C value
+//            celsiusLabel.text = "\(celsiusValue.value)"
+            // Use the number formatter to set the value
+            celsiusLabel.text = numberFormatter.string(from: NSNumber(value: celsiusValue.value))
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+//        print("Current text: \(String(describing: textField.text))")
+//        print("Replacement text: \(string)")
+//        return true
+//        // Check for repeating radix points
+//        let existingTextHasDecimalSeparator = textField.text?.range(of: ".")
+//        let replacementTextHasDecimalSeparator = string.range(of: ".")
+        // Use Locale Separator
+        let currentLocal = Locale.current
+        let decimalSeparator = currentLocal.decimalSeparator ?? "."
+        let existingTextHasDecimalSeparator = textField.text?.range(of: decimalSeparator)
+        let replacementTextHasDecimalSeparator = string.range(of: decimalSeparator)
+        if existingTextHasDecimalSeparator != nil,
+            replacementTextHasDecimalSeparator != nil {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateCelsiusLabel()
+        print("Conversion Loaded")
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Ch7/MapViewController.swift
@@ -0,0 +1,86 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Daniel Salazar on 11/30/19.
+//  Copyright © 2019 Daniel Salazar. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    var mapView: MKMapView!
+    
+    // Programmatically create an MKMapView with constraints
+    override func loadView() {
+        // Create a map view
+        mapView = MKMapView()
+        // Set mapView as the root view
+        view = mapView
+        
+//        // Create segmentedControl
+//        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satelite"])
+        // Create segmentedControl with Localized Strings
+        let standardString = NSLocalizedString("Standard", comment: "Standard map view")
+        let hybridString = NSLocalizedString("Hybrid", comment: "Hybrid map view")
+        let sateliteString = NSLocalizedString("Satelite", comment: "Satelite map view")
+        let segmentedControl = UISegmentedControl(items: [standardString, hybridString, sateliteString])
+        // Set background to white
+        segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        // Start selection on "Standard"
+        segmentedControl.selectedSegmentIndex = 0
+        // Turn off default translation
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        // Add the segmentedControl to the view
+        view.addSubview(segmentedControl)
+        
+//         Create layout constraints for segmentedControl manually
+//        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: view.topAnchor)
+//        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+//        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        // Create layout constraints using safee area layout guide
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8)
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        
+        // Activate constraints for segmentedControl
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+        
+        segmentedControl.addTarget(self, action: #selector(MapViewController.mapTypeChanged(_:)), for: .valueChanged)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Map loaded")
+        // Do any additional setup after loading the view.
+    }
+    
+    @objc func mapTypeChanged(_ segControl: UISegmentedControl) {
+        switch segControl.selectedSegmentIndex {
+        case 0:
+            mapView.mapType = .standard
+        case 1:
+            mapView.mapType = .hybrid
+        case 2:
+            mapView.mapType = .satellite
+        default:
+             break
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
