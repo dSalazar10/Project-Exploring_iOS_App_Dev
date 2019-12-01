67fd11dc6c832e19fd76909dcab0e0943c9ec6e6
Daniel Salazar <dsalazar10@horizon.csueastbay.edu>
2019-11-30 22:04:47 -0800

    Add files via upload

---
+++ Ch5/MapViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Daniel Salazar on 11/30/19.
+//  Copyright © 2019 Daniel Salazar. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Map loaded")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
